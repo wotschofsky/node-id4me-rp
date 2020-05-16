ae40046363e81e141c3ca0bcf888a371273dfcf8
Felix Wotschofsky <hello@felisk.io>
2020-05-16 17:08:40 +0200

    Refactor registration.ts 💪

--- src/utils/registration.ts
+++ src/utils/registration.ts
@@ -4,15 +4,17 @@ import { defaultStorageAdapter } from '../ApplicationStorageAdapter';
 import { AuthorityConfiguration, ApplicationRegistrationData, ApplicationResponse } from '../types';
 
 const removeTrailingSlash = (value: string): string => {
+  // Test if last character is "/"
   if (value.endsWith('/')) {
+    // Remove last character
     return value.slice(0, -1);
   }
+
+  // Fallback
   return value;
 };
 
 export const getConfigurationUrl = (iss: string): string => {
-  if (!iss) return iss;
-
   const strippedUrl = removeTrailingSlash(iss);
   const result = `${strippedUrl}/.well-known/openid-configuration`;
   return result;
@@ -20,21 +22,26 @@ export const getConfigurationUrl = (iss: string): string => {
 
 export const getConfiguration = async (iss: string, forceRefetch = false): Promise<AuthorityConfiguration> => {
   const cacheKey = `authorityConfig.${iss}`;
-  const existsInCache = cache.has(cacheKey) && !forceRefetch;
 
-  if (existsInCache) {
+  // If configuration object is available in cache get and return it
+  if (cache.has(cacheKey) && !forceRefetch) {
     const config = cache.get(cacheKey) as AuthorityConfiguration;
-    if (config) {
-      return config;
-    }
+    return config;
   }
 
   try {
-    const response = await axios.get(getConfigurationUrl(iss));
+    // Get Identity Agent config
+    const endpoint = getConfigurationUrl(iss);
+    const response = await axios.get(endpoint);
+
+    // Test if the responding agent matches the requested one
     if (removeTrailingSlash(response.data.issuer) !== removeTrailingSlash(iss)) {
       throw new Error('Issuer does not match requested one');
     }
+
+    // Put response into the cache
     cache.set(cacheKey, response.data);
+
     return response.data;
   } catch (error) {
     console.log(error);
@@ -48,6 +55,7 @@ export const registerApplication = async (
   adapter = defaultStorageAdapter,
   forceReset = false
 ): Promise<ApplicationResponse> => {
+  // Try to get config from storage if forceReset is false
   if (!forceReset) {
     const savedData = await adapter.get(iss);
     if (savedData) {
@@ -55,26 +63,36 @@ export const registerApplication = async (
     }
   }
 
-  if (config.application_type === 'web') {
+  if (config.application_type === 'web' || !config.application_type) {
     for (const uri of config.redirect_uris) {
+      // Prevent of the URIs being localhost
       if (/localhost/.test(uri)) {
         throw new Error('redirect_uris may not include "localhost" for web applications');
       }
+
+      // Prevent non-https URIs
       if (!uri.startsWith('https:')) {
         throw new Error('redirect_uris for web applications need to be using the https scheme');
       }
     }
   }
 
+  // Prevent sending both jwks_uri and jwks properties
   if (config.jwks_uri && config.jwks) {
     throw new Error('jwks_uri and jwks may not be defined at the same time');
   }
 
-  const endpoint = (await getConfiguration(iss))['registration_endpoint'] as string;
-
   try {
+    // Extract registration endpoint from config
+    const authorityConfig = await getConfiguration(iss);
+    const endpoint = authorityConfig.registration_endpoint;
+
+    // Register application with Identity Agent
     const response = await axios.post(endpoint, config);
+
+    // Save response using ApplicationStorageAdapter
     adapter.save(iss, response.data);
+
     return response.data;
   } catch (error) {
     console.log(error);

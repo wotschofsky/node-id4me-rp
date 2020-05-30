9f31b8ca9174916fe2e558704654b00cff19ab02
Felix Wotschofsky <hello@felisk.io>
2020-05-30 19:32:54 +0200

    Add Google Firestore storage adapter 🔥

--- demo/storageAdapters.ts
+++ demo/storageAdapters.ts
@@ -1,5 +1,6 @@
 import { ApplicationResponse } from 'id4me-rp/lib/types';
 import { ApplicationStorageAdapter } from 'id4me-rp';
+import { Firestore } from '@google-cloud/firestore';
 import { MongoClient, Db } from 'mongodb';
 import dotenv from 'dotenv';
 import FileAsync from 'lowdb/adapters/FileAsync';
@@ -114,5 +115,70 @@ export const mongoStorageAdapter = new ApplicationStorageAdapter(
 
     // Return true if MongoDB executed deletion successfully
     return result.result.ok === 1;
+  }
+);
+
+let firestoreInstance: Firestore | null = null;
+const getFirestoreInstance = (): Firestore => {
+  if (!firestoreInstance) {
+    const config: Record<string, string> = {};
+    if (process.env.GCP_PROJECT && process.env.GCP_CREDENTIALS_FILE) {
+      config.projectId = process.env.GCP_PROJECT;
+      config.keyFilename = process.env.GCP_CREDENTIALS_FILE;
+    }
+
+    const instance = new Firestore(config);
+    firestoreInstance = instance;
+    return instance;
+  }
+
+  return firestoreInstance;
+};
+
+export const firestoreStorageAdapter = new ApplicationStorageAdapter(
+  async (identifier, data) => {
+    const firestore = getFirestoreInstance();
+    const collection = firestore.collection(APPLICATIONS_COLLECTION);
+
+    try {
+      await collection.add({
+        identifier: identifier,
+        data: data
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  async (identifier): Promise<ApplicationResponse | null> => {
+    const firestore = getFirestoreInstance();
+    const collection = firestore.collection(APPLICATIONS_COLLECTION);
+
+    try {
+      const querySnapshot = await collection.where('identifier', '==', identifier).limit(1).get();
+
+      if (querySnapshot.empty) {
+        return null;
+      }
+
+      const doc = querySnapshot.docs[0];
+      const content = doc.data();
+
+      return content.data;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  },
+  async (identifier) => {
+    const firestore = getFirestoreInstance();
+    const collection = firestore.collection(APPLICATIONS_COLLECTION);
+
+    const ref = await collection.where('identifier', '==', identifier).get();
+
+    ref.forEach((doc) => {
+      doc.ref.delete();
+    });
+
+    return true;
   }
 );
